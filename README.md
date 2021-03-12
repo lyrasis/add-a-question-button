9fbd8e6ebd17ca8f1023bbe752a2f962910659b7
Blake Carver <btcarver@lisnews.com>
2021-03-12 15:57:10 -0500

    Add some code and a little readme stuff

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # add-a-question-button
+
+Add to Locales PUBLIC en.yml:
+
+```
+en:
+  plugin:
+    help:
+      text: Ask The Archives A Question
+      url: https://www.example.org/Ask-the-Archivist
+```
---
+++ public/plugin_init.rb
@@ -0,0 +1,2 @@
+Plugins::extend_aspace_routes(File.join(File.dirname(__FILE__), "routes.rb"))
+Plugins::add_record_page_action_js(['resource', 'archival_object', 'digital_object', 'accession'], 'plugin.help.text', 'fa-question-circle', "window.open('#{I18n.t('plugin.help.url')}'), '_blank').focus();")
